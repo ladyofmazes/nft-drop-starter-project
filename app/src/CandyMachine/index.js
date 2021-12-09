1bc549cda6be58c3afa3cdf8cd002d2f79117f14
Melanie Day <mamday@gmail.com>
2021-12-08 18:56:15 -0800

    Add access to candy machine

--- app/src/CandyMachine/index.js
+++ app/src/CandyMachine/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Connection, PublicKey } from '@solana/web3.js';
 import { Program, Provider, web3 } from '@project-serum/anchor';
 import { MintLayout, TOKEN_PROGRAM_ID, Token } from '@solana/spl-token';
@@ -25,6 +25,9 @@ const MAX_SYMBOL_LENGTH = 10;
 const MAX_CREATOR_LEN = 32 + 1 + 1;
 
 const CandyMachine = ({ walletAddress }) => {
+  // Add state property inside your component like this
+  const [machineStats, setMachineStats] = useState(null);
+
   useEffect(() => {
     getCandyMachineState();
   }, []);	
@@ -70,6 +73,15 @@ const CandyMachine = ({ walletAddress }) => {
       goLiveData * 1000
     ).toGMTString()}`
 
+    // Add this data to your state to render
+    setMachineStats({
+      itemsAvailable,
+      itemsRedeemed,
+      itemsRemaining,
+      goLiveData,
+      goLiveDateTimeString,
+    });
+
     console.log({
       itemsAvailable,
       itemsRedeemed,
@@ -77,6 +89,18 @@ const CandyMachine = ({ walletAddress }) => {
       goLiveData,
       goLiveDateTimeString,
     });
+    return (
+      // Only show this if machineStats is available
+      machineStats && (
+        <div className="machine-container">
+          <p>{`Drop Date: ${machineStats.goLiveDateTimeString}`}</p>
+          <p>{`Items Minted: ${machineStats.itemsRedeemed} / ${machineStats.itemsAvailable}`}</p>
+          <button className="cta-button mint-button" onClick={null}>
+              Mint NFT
+          </button>
+        </div>
+      )
+    );
   };
 
   // Actions
